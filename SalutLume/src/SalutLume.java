7360bb58b28064657f5d0966f700762078695f53
Anna <Anna@Anna-PC>
2017-11-12 19:07:23 +0200

    Prima modificare

--- SalutLume/src/SalutLume.java
+++ SalutLume/src/SalutLume.java
@@ -14,5 +14,6 @@ public class SalutLume {
   public static void main(String[] args) {
     System.out.println("Primul programa JAVA!");
     System.out.println("Data, ora: "+ new Date());
+    System.out.println("Test1")
   }
 } ///:~
