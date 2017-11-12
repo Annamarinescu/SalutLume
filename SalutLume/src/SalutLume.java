e6dc2c9c7d6beab9b880c5eee4c77abc3588c4c8
Anna <Anna@Anna-PC>
2017-11-12 18:24:16 +0200

    Realizare commit

---
+++ SalutLume/src/SalutLume.java
@@ -0,0 +1,18 @@
+//: SalutLume.java
+import java.util.*;
+
+/** Descriere functionalitate: afisare mesaj de salut si data.
+ * @author Bruce Eckel
+ * @version 2.0
+*/
+public class SalutLume {
+  /** 
+   * @param args -- vector de siruri de caractere
+   * @return Nu exista valori intoarse de metoda main
+   * @exception Nu sunt aruncate exceptii explicite
+  */
+  public static void main(String[] args) {
+    System.out.println("Primul programa JAVA!");
+    System.out.println("Data, ora: "+ new Date());
+  }
+} ///:~
